053781616a82f9e3471ac2665c11c06cde380204
Alvaro Zárate <71851520+AlvaroZarateO@users.noreply.github.com>
2021-12-20 15:15:15 -0500

    Update mef_proyectos.py

--- mef_proyectos.py
+++ mef_proyectos.py
@@ -10,6 +10,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
+
 ##############################################################################
 ############# GOBIERNO NACIONAL, REGIONAL Y LOCAL (AGREGADOS) ################
 
